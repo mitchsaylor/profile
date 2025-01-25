69ce73d720c5bd90c2e6b41f0295b2f9108e54ab
mitchsaylor <mitch@saylor.io>
2025-01-25 10:40:26 -0500

    build: add tailwindcss

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	let { children } = $props();
+
+	// CSS
+	import '../app.css';
+</script>
+
+<div class="min-h-dvh flex flex-col">
+	<main class="grow">
+		{@render children()}
+	</main>
+</div>
